fe2ff35a2ce7f7723caafc61bd747cd4407217b5
zpj <2325339330@qq.com>
2025-09-22 17:48:51 +0800

    更新 ChromaDB 初始化逻辑

--- agents/MessageManager.py
+++ agents/MessageManager.py
@@ -7,7 +7,7 @@ class MessagerManager:
     分层消息管理器
     """
  
-    def __init__(self, max_woking_memory=100, max_history=500):
+    def __init__(self, max_woking_memory=1000, max_history=5000):
         self.max_woking_memory = max_woking_memory
         self.max_history = max_history
         self.history = []
